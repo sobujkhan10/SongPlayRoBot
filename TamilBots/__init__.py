658617e15251a52fdde136492b5b0c6e930bd57f
sobujkhan10 <87231017+sobujkhan10@users.noreply.github.com>
2021-07-10 18:41:24 +0600

    Update __init__.py

--- TamilBots/__init__.py
+++ TamilBots/__init__.py
@@ -8,4 +8,4 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-app = Client("SongPlayRoBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+app = Client("MusicDownloadersBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
